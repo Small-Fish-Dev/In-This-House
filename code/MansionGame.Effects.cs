e6f35c3711478de13017a7cb6113444ee56cb9b4
ceitine <ceitinee@gmail.com>
2023-10-14 17:09:46 +0300

    vignette from s&box screeneffects

--- code/MansionGame.Effects.cs
+++ code/MansionGame.Effects.cs
@@ -6,12 +6,18 @@ partial class MansionGame
 {
 	public ScreenEffects ScreenEffects { get; private set; }
 
+	[Event.Hotload]
 	protected void InitializeEffects()
 	{
+		if ( Game.IsServer )
+			return;
+
 		// s&box's own post processing effects, we might want to use chromatic aberration and vignette.
 		ScreenEffects = Camera.Main.FindOrCreateHook<ScreenEffects>();
+		ScreenEffects.Vignette.Intensity = 0.4f;
 
 		// dithering
 		var dithering = Camera.Main.FindOrCreateHook<DitheringEffect>();
+		dithering.Order = -1;
 	}
 }
