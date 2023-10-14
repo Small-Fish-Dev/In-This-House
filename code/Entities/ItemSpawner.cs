0a688b1d56f8e7dae06394b7b4c17c4fec36c599
duston <>
2023-10-13 21:39:43 -0500

    Add stub ItemSpawner entity

---
+++ code/Entities/ItemSpawner.cs
@@ -0,0 +1,27 @@
+﻿using Editor;
+
+namespace BrickJam;
+
+[HammerEntity]
+[EditorModel( "models/editor/axis_helper_thick.vmdl" )]
+public partial class ItemSpawner : Entity
+{
+	// TODO: Change this with whatever we end up using for items. (enum, string id, prefab, string path to prefab/resource, etc)
+	[Property]
+	public Prefab ItemToSpawn { get; set; }
+
+	public override void Spawn()
+	{
+		base.Spawn();
+		Transmit = TransmitType.Never;
+
+		if ( !PrefabLibrary.TrySpawn<Entity>( ItemToSpawn.ResourcePath, out var item ) )
+		{
+			Log.Error( $"{this} Couldn't spawn item! item: {ItemToSpawn}" );
+			return;
+		}
+
+		item.Position = Position;
+		item.Rotation = Rotation;
+	}
+}
