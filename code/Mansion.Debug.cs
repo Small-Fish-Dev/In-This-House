912626b3906af276a625b0f66651334d7de16df2
yuberee <59583743+yuberee@users.noreply.github.com>
2023-10-14 02:29:38 +0200

    set_state debug

---
+++ code/Mansion.Debug.cs
@@ -0,0 +1,11 @@
+﻿namespace BrickJam;
+
+public partial class Mansion : GameManager
+{
+	[ConCmd.Admin( "set_state" )]
+	public static void SetState( string state )
+	{
+		if ( state == "play" || state == "playing" )
+			SetState<PlayingState>();
+	}
+}
