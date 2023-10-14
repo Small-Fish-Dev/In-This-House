0e05d2841a63a72c2a2df7dbc405440f6e5dcd52
yuberee <59583743+yuberee@users.noreply.github.com>
2023-10-14 15:19:26 +0200

    PlayerSpawn

---
+++ code/Entities/PlayerSpawn.cs
@@ -0,0 +1,12 @@
+﻿using Editor;
+
+namespace BrickJam;
+
+[HammerEntity]
+[EditorModel( "models/citizen/citizen.vmdl" )]
+public partial class PlayerSpawn : Entity
+{
+	[Property] public LevelType LevelType { get; set; } = LevelType.None;
+
+	public PlayerSpawn() => Transmit = TransmitType.Never;
+}
