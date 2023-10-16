a6e417ac35c6686b89ab83f6ba44c590152e98b6
yuberee <59583743+yuberee@users.noreply.github.com>
2023-10-17 00:33:44 +0200

    Rarity chances on different levels

--- code/Entities/ItemSpawner.cs
+++ code/Entities/ItemSpawner.cs
@@ -10,6 +10,52 @@ public partial class ItemSpawner : Entity
 	[Property]
 	public ItemPrefab ItemToSpawn { get; set; }
 
+	public static Dictionary<LevelType, Dictionary<ItemRarity, float>> RarityChances { get; set; } = new()
+	{
+		{
+			LevelType.Mansion, new Dictionary<ItemRarity, float>()
+			{
+				{ ItemRarity.Broken, 1f },
+				{ ItemRarity.Decrepit, 1.2f },
+				{ ItemRarity.Worn, 1.4f },
+				{ ItemRarity.Dusty, 1.2f },
+				{ ItemRarity.Common, 1f },
+				{ ItemRarity.Nice, 0.6f },
+				{ ItemRarity.Great, 0.3f },
+				{ ItemRarity.Excellent, 0.1f },
+				{ ItemRarity.Flawless, 0.03f }
+			}
+		},
+		{
+			LevelType.Dungeon, new Dictionary<ItemRarity, float>()
+			{
+				{ ItemRarity.Broken, 0.5f },
+				{ ItemRarity.Decrepit, 0.7f },
+				{ ItemRarity.Worn, 0.9f },
+				{ ItemRarity.Dusty, 1f },
+				{ ItemRarity.Common, 1.2f },
+				{ ItemRarity.Nice, 1f },
+				{ ItemRarity.Great, 0.7f },
+				{ ItemRarity.Excellent, 0.3f },
+				{ ItemRarity.Flawless, 0.1f }
+			}
+		},
+		{
+			LevelType.Bathrooms, new Dictionary<ItemRarity, float>()
+			{
+				{ ItemRarity.Broken, 0.2f },
+				{ ItemRarity.Decrepit, 0.3f },
+				{ ItemRarity.Worn, 0.4f },
+				{ ItemRarity.Dusty, 0.6f },
+				{ ItemRarity.Common, 0.9f },
+				{ ItemRarity.Nice, 1.2f },
+				{ ItemRarity.Great, 1.6f },
+				{ ItemRarity.Excellent, 1.2f },
+				{ ItemRarity.Flawless, 0.9f }
+			}
+		},
+	};
+
 	public override void Spawn()
 	{
 		base.Spawn();
