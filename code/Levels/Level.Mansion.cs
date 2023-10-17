959b2e1a319ef7fbbf005d7009ab6a510269d1d3
yuberee <59583743+yuberee@users.noreply.github.com>
2023-10-18 00:33:21 +0200

    Update mansion bounds

--- code/Levels/Level.Mansion.cs
+++ code/Levels/Level.Mansion.cs
@@ -3,7 +3,7 @@
 public partial class MansionLevel : Level
 {
 	public override LevelType Type { get; set; } = LevelType.Mansion;
-	public override BBox WorldBox => new BBox( new Vector3( -1550f, -1550f, 500f ), new Vector3( 1550f, 1550f, 640f ) );
+	public override BBox WorldBox => new BBox( new Vector3( -2330f, -1320f, 450f ), new Vector3( 1600f, 1320f, 730f ) );
 
 	public async override Task Start()
 	{
