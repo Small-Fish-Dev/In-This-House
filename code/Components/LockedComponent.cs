97ad6a2e3b9edd4dbd482f69bc3ad350fdab7fe2
ceitine <ceitinee@gmail.com>
2023-10-19 22:50:49 +0300

    lockedcomponent and lockpicker ui progress

---
+++ code/Components/LockedComponent.cs
@@ -0,0 +1,47 @@
+﻿namespace BrickJam;
+
+public partial class LockedComponent : EntityComponent
+{
+	[Net] public bool Locked { get; set; }
+
+	public void Lockpick()
+	{
+		if ( !Locked )
+			return;
+
+		Game.AssertServer();
+
+		_lockpick( Entity.NetworkIdent );
+	}
+
+	public void RequestLockpicked()
+		=> _requestLockpicked( Entity.NetworkIdent );
+
+	[ConCmd.Server( "request_lockpicked" )]
+	public static void _requestLockpicked( int ident )
+	{
+		var entity = Entity.FindByIndex( ident );
+		if ( entity == null )
+			return;
+
+		var component = entity.Components.Get<LockedComponent>();
+		if ( component == null )
+			return;
+
+		component.Locked = false;
+	}
+
+	[ClientRpc]
+	public void _lockpick( int ident )
+	{
+		var entity = Entity.FindByIndex( ident );
+		if ( entity == null )
+			return;
+	
+		var component = entity.Components.Get<LockedComponent>();
+		if ( component == null )
+			return;
+
+		UI.Lockpicker.Open( component );
+	}
+}
