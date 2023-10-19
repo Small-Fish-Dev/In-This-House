61670c6de387640f226efb328410645eae7c7c27
yuberee <59583743+yuberee@users.noreply.github.com>
2023-10-19 21:13:01 +0200

    Shop entities

---
+++ code/Entities/Shop.cs
@@ -0,0 +1,27 @@
+﻿using Editor;
+
+namespace BrickJam;
+
+[HammerEntity]
+[EditorModel( "models/containers/chest/chest.vmdl" )]
+public class Shop : UsableEntity
+{
+	public override float InteractionDuration => 0f;
+	public override string UseString => "open the upgrades shop";
+
+	public override void Spawn()
+	{
+		base.Spawn();
+
+		SetModel( "models/containers/chest/chest.vmdl" );
+		SetupPhysicsFromModel( PhysicsMotionType.Keyframed );
+		Tags.Add( "solid" );
+	}
+
+	public override void Use( Player user )
+	{
+		base.Use( user );
+
+		// OPEN THE SHOPPE
+	}
+}
---
+++ code/Entities/ShopDoor.cs
@@ -0,0 +1,30 @@
+﻿using Editor;
+
+namespace BrickJam;
+
+[HammerEntity]
+[EditorModel( "models/furniture/mansion_furniture/mansion_door.vmdl" )]
+public class ShopDoor : UsableEntity
+{
+	public override float InteractionDuration => 2f;
+	public override string UseString => CanUse ? "enter the mansion" : "ALL PLAYERS NEED TO BE NEARBY TO PROCEED";
+	public override bool CanUse => Entity.All.OfType<Player>()
+		.Where( x => x.IsAlive )
+		.All( x => x.Position.Distance( Position ) <= 300f );
+
+	public override void Spawn()
+	{
+		base.Spawn();
+
+		SetModel( "models/furniture/mansion_furniture/mansion_door.vmdl" );
+		SetupPhysicsFromModel( PhysicsMotionType.Keyframed );
+		Tags.Add( "door" );
+	}
+
+	public override void Use( Player user )
+	{
+		base.Use( user );
+
+		MansionGame.NextLevel();
+	}
+}
