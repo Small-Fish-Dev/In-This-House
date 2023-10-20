c47bf97ec3ea6953d9b850df91d46b7d10b6638a
yuberee <59583743+yuberee@users.noreply.github.com>
2023-10-20 03:15:22 +0200

    FinalDoor and ValidFinalDoorPosition

---
+++ code/Entities/FinalDoor.cs
@@ -0,0 +1,28 @@
+﻿using Editor;
+
+namespace BrickJam;
+
+public class FinalDoor : UsableEntity
+{
+	public override float InteractionDuration => 2f;
+	public override string UseString => CanUse ? "exit the mansion" : "ALL PLAYERS NEED TO BE NEARBY TO PROCEED";
+	public override bool CanUse => Entity.All.OfType<Player>()
+		.Where( x => x.IsAlive )
+		.All( x => x.Position.Distance( Position ) <= 300f );
+
+	public override void Spawn()
+	{
+		base.Spawn();
+
+		SetModel( "models/furniture/mansion_furniture/mansion_door.vmdl" );
+		SetupPhysicsFromModel( PhysicsMotionType.Keyframed );
+		Tags.Add( "door" );
+	}
+
+	public override void Use( Player user )
+	{
+		base.Use( user );
+
+		MansionGame.RestartGame(); // TODO: Give bonus / Show ending
+	}
+}
---
+++ code/Entities/ValidFinalDoorPosition.cs
@@ -0,0 +1,10 @@
+﻿using Editor;
+
+namespace BrickJam;
+
+[HammerEntity]
+[EditorModel( "models/furniture/mansion_furniture/mansion_door.vmdl" )]
+public partial class ValidFinalDoorPosition : Entity
+{
+	public ValidFinalDoorPosition() => Transmit = TransmitType.Never;
+}
