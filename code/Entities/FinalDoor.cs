cd8f7b2a98ee115d8063e352afeb0a59cee06758
Gio <lotuspar0@gmail.com>
2023-10-21 22:00:11 +0800

    Change interaction durations

--- code/Entities/FinalDoor.cs
+++ code/Entities/FinalDoor.cs
@@ -4,7 +4,7 @@ namespace BrickJam;
 
 public class FinalDoor : UsableEntity
 {
-	public override float InteractionDuration => 2f;
+	public override float InteractionDuration => 1.7f;
 	public override string UseString => CanUse ? "exit the mansion" : "ALL PLAYERS NEED TO BE NEARBY TO PROCEED";
 	public override bool CanUse => Entity.All.OfType<Player>()
 		.Where( x => x.IsAlive )
@@ -18,11 +18,10 @@ public class FinalDoor : UsableEntity
 		SetupPhysicsFromModel( PhysicsMotionType.Keyframed );
 		Tags.Add( "door" );
 	}
-
+	
 	public override void Use( Player user )
 	{
 		base.Use( user );
-
 		MansionGame.RestartGame(); // TODO: Give bonus / Show ending
 	}
 }
--- code/Entities/Loot/Loot.cs
+++ code/Entities/Loot/Loot.cs
@@ -8,6 +8,7 @@ public partial class Loot : UsableEntity
 	
 	public override string UseString => $"take the {FullName}";
 
+	public override float InteractionDuration => 0;
 	[Net] public LootRarity Rarity { get; set; } = LootRarity.Common;
 	[Net] public int BaseMonetaryValue { get; set; } = 0;
 	[Net] public string BaseName { get; set; } = "Loot";
