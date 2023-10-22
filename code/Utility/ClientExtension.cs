1e77d3542a70406d90711d22b730b353c1666c23
Ivan Kuzmenko <6745157+rndtrash@users.noreply.github.com>
2023-10-22 13:39:33 +0300

    limit the size of a card, s&box moment

--- code/Utility/ClientExtension.cs
+++ code/Utility/ClientExtension.cs
@@ -2,7 +2,17 @@
 
 public static class ClientExtension
 {
-	public static int GetSlot( this IClient client ) => client?.GetInt( "slot", -1 ) ?? -1;
+	public static int GetSlot( this IClient client )
+	{
+		var slot = client?.GetInt( "slot", -1 ) ?? -1;
+		if ( slot >= Game.Server.MaxPlayers )
+		{
+			Log.Error( "S&BOX MOMENT!!!" );
+			return -1;
+		}
+
+		return slot;
+	}
 
 	public static Color GetColor( this IClient client )
 	{
