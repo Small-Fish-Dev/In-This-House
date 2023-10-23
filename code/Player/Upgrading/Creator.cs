19ffc7341baf92a4c1a9c8532bfa2dd750295cef
yuberee <59583743+yuberee@users.noreply.github.com>
2023-10-23 17:18:29 +0200

    Fix upgrade descriptio nand prices

--- code/Player/Upgrading/Creator.cs
+++ code/Player/Upgrading/Creator.cs
@@ -13,49 +13,49 @@ public static class Creator
 			.ConfigureWith( v =>
 				v.FrictionMultiplier = 2f )
 			.WithPrice( 500 )
-			// .WithTexture( "ui/icon/jellyfish-jam.png" )
+			.WithTexture( "ui/upgrades/work_shoes.png", true )
 			.Build();
 
 		new Upgrade.Builder( "Cartoony Sidekick", "Summon Doob the Dog to protect you" )
 			.ConfigureWith( v =>
 				v.Doob = 1f )
 			.WithPrice( 1000 )
-			// .WithTexture( "ui/icon/jellyfish-jam.png" )
+			.WithTexture( "ui/upgrades/doob.png", true )
 			.Build();
 
 		new Upgrade.Builder( "Mansion Key", "The key needed to unlock the trapdoor found in the mansion" )
 			.ConfigureWith( v =>
 				v.KeyToDungeon = 1f )
 			.WithPrice( 800 )
-			// .WithTexture( "ui/icon/jellyfish-jam.png" )
+			.WithTexture( "ui/upgrades/mansionkey.png", true )
 			.Build();
 
 		new Upgrade.Builder( "Faster Use", "Interacting with items takes less time" )
 			.ConfigureWith( v =>
 				v.UseMultiplier = 2f )
 			.WithPrice( 1100 )
-			// .WithTexture( "ui/icon/jellyfish-jam.png" )
+			.WithTexture( "ui/upgrades/fasteruse.png", true )
 			.Build();
 		
-		new Upgrade.Builder( "Dungeon Key", "The key needed to unlock the trapdoor found in the mansion" )
+		new Upgrade.Builder( "Dungeon Key", "The key needed to unlock the trapdoor found in the dungeon" )
 			.ConfigureWith( v =>
 				v.KeyToBathrooms = 1f )
-			.WithPrice( 1600 )
-			// .WithTexture( "ui/icon/jellyfish-jam.png" )
+			.WithPrice( 1800 )
+			.WithTexture( "ui/upgrades/dungeonkey.png", true )
 			.Build();
 
 		new Upgrade.Builder( "Lock Breaker", "No need to lockpick, just break the lock" )
 			.ConfigureWith( v =>
 				v.BreakLocks = 1f )
-			.WithPrice( 1800 )
-			// .WithTexture( "ui/icon/jellyfish-jam.png" )
+			.WithPrice( 2400 )
+			.WithTexture( "ui/upgrades/lockbreaker.png", true )
 			.Build();
 		
 		new Upgrade.Builder( "Exit Key", "The key needed to exit from this cursed place" )
 			.ConfigureWith( v =>
 				v.KeyToExit = 1f )
-			.WithPrice( 2200 )
-			// .WithTexture( "ui/icon/jellyfish-jam.png" )
+			.WithPrice( 4400 )
+			.WithTexture( "ui/upgrades/exitkey.png", true )
 			.Build();
 	}
 }
