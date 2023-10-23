6749285f7e7fe0ef745c1fe1dfddcbcc9c2bd232
Gio <lotuspar0@gmail.com>
2023-10-23 22:34:12 +0800

    Update BookPrefab.cs

--- code/Entities/Loot/BookPrefab.cs
+++ code/Entities/Loot/BookPrefab.cs
@@ -1,7 +1,11 @@
-﻿namespace BrickJam;
+﻿using System.Text.Json.Serialization;
+
+namespace BrickJam;
 
 [GameResource( "BookPrefab", "book", "Define book data.", Icon = "star" )]
 public class BookPrefab : LootPrefab
 {
 	public string Text { get; set; }
+
+	[ResourceType( "vmdl" )] public new string Model { get; set; } = "models/sbox_props/watermelon/watermelon_gib08.vmdl";
 }
