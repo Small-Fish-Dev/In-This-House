a6707001af20de1331212e25a8d1fe269d144f0e
Gio <lotuspar0@gmail.com>
2023-10-23 09:16:13 +0800

    Add the money waft

--- code/Entities/SellArea.cs
+++ code/Entities/SellArea.cs
@@ -1,10 +1,11 @@
-﻿using Editor;
+﻿using BrickJam.UI;
+using Editor;
 
 namespace BrickJam;
 
 [HammerEntity]
 [Title( "Sell Area" ), Category( "Trigger" ), Icon( "place" )]
-public class SellArea : BaseTrigger
+public partial class SellArea : BaseTrigger
 {
 	public SellArea()
 	{
@@ -27,6 +28,18 @@ public class SellArea : BaseTrigger
 		Eventlog.Send( $"You sold the <gray>{loot.FullName}<white> for <green>${loot.MonetaryValue}.",
 			To.Single( loot.LastPlayer ) );
 
+		OnSellRpc( To.Everyone, loot.Position, loot.MonetaryValue );
+		
 		loot.Delete();
+	}
+
+	[ClientRpc]
+	public static void OnSellRpc( Vector3 position, int value )
+	{
+		for ( var i = 0; i < 10; i++ )
+		{
+			var waft = new MoneyWaft( value );
+			waft.Position = position;
+		}
 	}
 }
