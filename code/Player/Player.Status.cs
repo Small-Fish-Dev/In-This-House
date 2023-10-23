5621dce2d868894a8f63ca39ce7e47dcc0ea7a6c
yuberee <59583743+yuberee@users.noreply.github.com>
2023-10-23 15:49:15 +0200

    Change crash sound

--- code/Player/Player.Status.cs
+++ code/Player/Player.Status.cs
@@ -18,7 +18,7 @@ public partial class Player
 		var volume = MathX.Remap( multiplier, 0.1f, 1f, 0.3f, 1f );
 		var pitch = MathX.Remap( multiplier, 0.1f, 1f, 1.4f, 0.5f );
 
-		PlaySound( "sounds/pipe.sound" )
+		PlaySound( "sounds/crash/crash.sound" )
 			.SetVolume( multiplier );
 
 		StunLeft = StunDuration * multiplier;
